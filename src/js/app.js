3ec2bfc59fbdd9e54975893b753d9e11b482db8b
marselgray <marselgray@gmail.com>
2020-11-03 19:29:53 -0500

    🚀 Update: csv file

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,33 @@ console.log('hi');
 
 d3.csv('https://raw.githubusercontent.com/marselgray/senate_map/main/data/data.csv').then(function(data) {
 	console.log(data);
+
+	for(let i = 0; i < data.length; i++){
+		// possible outs: 
+		// state is R
+		if(data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'R'){
+			console.log(data[i]['state'] + ` has two republican senators`);
+		} 
+		// state is D
+		else if (data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'D') {
+			console.log(data[i]['state'] + ` has two democratic senators`);
+		} 
+		// state is R and D or state is D and R
+		else if ((data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'D') || (data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'R')) {
+			console.log(data[i]['state'] + ` has one democratic and one republic senator`);
+		} 
+		// state is I and R or state is R and I
+		else if ((data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'I') || (data[i]['senator_one'] === 'I' && data[i]['senator_two'] === 'R')) {
+			console.log(data[i]['state'] + ` has one independent and one republic senator`);
+		}
+		// state is I and D or state is D and I
+		else if ((data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'I') || (data[i]['senator_one'] === 'I' && data[i]['senator_two'] === 'D')) {
+			console.log(data[i]['state'] + ` has one independent and one democratic senator`);
+		}
+		else {
+			console.log(data[i]['state'] + `examine this state`);
+		}
+	}
+
 });
 
