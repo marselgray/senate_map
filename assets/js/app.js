91eb012d35b5bde5337378c5534494a5a0f688a2
marselgray <marselgray@gmail.com>
2020-11-09 21:40:45 -0500

    🚀 Update: added map array functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,6 @@
 // grab data from svg
 d3.csv('https://raw.githubusercontent.com/marselgray/senate_map/main/data/data.csv').then(function(data) {
 
-
 	// grab states
 	var states = document.getElementsByClassName('map--state');
 
@@ -12,47 +11,51 @@ d3.csv('https://raw.githubusercontent.com/marselgray/senate_map/main/data/data.c
 	var independents = [];
 
 	// add color code to states
-	for(let i = 0; i < data.length; i++){
+	let i = 0;
+	data.map(function(item){
 		// possible outs: 
 		// state is R
-		if(data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'R'){
-			// console.log(data[i]['state'] + ` has two republican senators`);
+		if(item['senator_one'] === 'R' && item['senator_two'] === 'R'){
+			// console.log(item['state'] + ` has two republican senators`);
 			states[i].classList.add('map--red');
 			republicans.push('R');
 			republicans.push('R');
 		} 
 		// state is D
-		else if (data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'D') {
-			// console.log(data[i]['state'] + ` has two democratic senators`);
+		else if (item['senator_one'] === 'D' && item['senator_two'] === 'D') {
+			// console.log(item['state'] + ` has two democratic senators`);
 			states[i].classList.add('map--blue');
 			democrats.push('D');
 			democrats.push('D');
 		} 
 		// state is R and D or state is D and R
-		else if ((data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'D') || (data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'R')) {
-			// console.log(data[i]['state'] + ` has one democratic and one republic senator`);
+		else if ((item['senator_one'] === 'R' && item['senator_two'] === 'D') || (item['senator_one'] === 'D' && item['senator_two'] === 'R')) {
+			// console.log(item['state'] + ` has one democratic and one republic senator`);
 			states[i].classList.add('map--purple');
 			republicans.push('R');
 			democrats.push('D');
 		} 
 		// state is I and R or state is R and I
-		else if ((data[i]['senator_one'] === 'R' && data[i]['senator_two'] === 'I') || (data[i]['senator_one'] === 'I' && data[i]['senator_two'] === 'R')) {
-			// console.log(data[i]['state'] + ` has one independent and one republic senator`);
+		else if ((item['senator_one'] === 'R' && item['senator_two'] === 'I') || (item['senator_one'] === 'I' && item['senator_two'] === 'R')) {
+			// console.log(item['state'] + ` has one independent and one republic senator`);
 			states[i].classList.add('map--green__red');
 			republicans.push('R');
 			independents.push('I');
 		}
 		// state is I and D or state is D and I
-		else if ((data[i]['senator_one'] === 'D' && data[i]['senator_two'] === 'I') || (data[i]['senator_one'] === 'I' && data[i]['senator_two'] === 'D')) {
-			// console.log(data[i]['state'] + ` has one independent and one democratic senator`);
+		else if ((item['senator_one'] === 'D' && item['senator_two'] === 'I') || (item['senator_one'] === 'I' && item['senator_two'] === 'D')) {
+			// console.log(item['state'] + ` has one independent and one democratic senator`);
 			states[i].classList.add('map--green__blue');
 			democrats.push('D');
 			independents.push('I');
 		}
 		else {
-			console.log(data[i]['state'] + `examine this state`);
+			console.log(item['state'] + `examine this state`);
 		}
-	}
+
+		i++;
+	});
+
 
 	/*
 	calculate the % of seats of republicans and independent and use remainder in linear 
@@ -64,11 +67,12 @@ d3.csv('https://raw.githubusercontent.com/marselgray/senate_map/main/data/data.c
 
 
 	// add event listener to states
-	for(let i = 0; i < states.length; i++){
-		states[i].addEventListener('click', function(){
+	var arrStates = Array.prototype.slice.call(states);
+	arrStates.map(function(item){
+		item.addEventListener('click', function(){
 			console.log(this.attributes[3].value);
 		})
-	}
+	})
 
 });
 
